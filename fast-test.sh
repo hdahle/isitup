f23014516b170492cf13f93f2f27cd4269b4faff
Hakon Dahle <hakon.dahle@gmail.com>
2020-05-05 21:29:48 +0200

    Fast-test: Added comments

--- fast-test.sh
+++ fast-test.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+# This is pretty much cloned from api.whereisitup.com
+# H. Dahle
+
 curl -i "https://api.wheresitup.com/v4/jobs" \
     -H "Auth: Bearer 5eac3ce040d03f563c201b0e 3e723771ee0443b812b3138e5fae3b4f" \
     -H "Content-Type: application/json" \
