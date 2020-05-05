56525a5b06c5ac2b1ca0831095d4e845b2d61fa0
Hakon Dahle <hakon.dahle@gmail.com>
2020-05-05 21:31:59 +0200

    Update sources.sh

--- sources.sh
+++ sources.sh
@@ -1,2 +1,9 @@
+#!/bin/sh
+# Get list of source servers
+# Code is from api.whereisitup.com
+# H.Dahle
+
 curl -i "https://api.wheresitup.com/v4/sources" \
   -H "Auth: Bearer 5eac3ce040d03f563c201b0e 3e723771ee0443b812b3138e5fae3b4f"
+
+echo ""
