59d1a8be911eed43eff6829722845c68ebc9d78b
Hakon Dahle <hakon.dahle@gmail.com>
2020-05-05 21:33:13 +0200

    Update checkyourcredits.sh

--- checkyourcredits.sh
+++ checkyourcredits.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+# Code is from api.whereisitup.com
+# H.Dahle
+
 curl -i "https://api.wheresitup.com/v4/credits" \
   -H "Auth: Bearer 5eac3ce040d03f563c201b0e 3e723771ee0443b812b3138e5fae3b4f"
 
