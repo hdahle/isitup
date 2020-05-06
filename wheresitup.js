3b3dbd1543afd8d5b3d6762e1f226ae50ec86f00
Håkon Dahle <hakon.dahle@gmail.com>
2020-05-06 12:43:13 +0200

    major refactoring of JS

--- wheresitup.js
+++ wheresitup.js
@@ -9,23 +9,30 @@ var moment = require('moment');
 var argv = require('minimist')(process.argv.slice(2));
 const momFmt = 'YY-MM-DD hh:mm:ss';
 
+const sources = ['seattle', 'newyork', 'bogota', 'london', 'oslo', 'amsterdam', 'frankfurt', 'stockholm', 'sydney', 'bangalore', 'bangkok', 'singapore', 'tokyo'];
+
 // process commandline
 const redisKey = argv.key;
 const jobURL = argv.url;
 const id = argv.id;
 const token = argv.token;
-const jobID = argv.job;
-if (redisKey === undefined || (jobURL === undefined && jobID === undefined) || id === undefined || token === undefined) {
+
+if (redisKey === undefined || jobURL === undefined || id === undefined || token === undefined) {
   console.log('Usage:\n\tnode wheresitup.js --key <rediskey> --id <id> --token <token> --url <http(s)://url>');
   console.log('\tnode wheresitup.js --key <rediskey> --id <id> --token <token> --job <jobID>');
   console.log('\n\tUse either --jobURL (to schedule a job) or --jobID (to retrieve results of a job), not both')
   return;
 }
 
+if (jobURL !== null) {
+  main();
+} else {
+  return;
+}
 
-// Schedule a job
-if (jobURL !== undefined && jobURL !== null) {
-  const options = {
+async function main() {
+  const wheresitupURL = 'https://api.wheresitup.com/v4/jobs';
+  const wheresitupStartOptions = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -34,46 +41,116 @@ if (jobURL !== undefined && jobURL !== null) {
     body: JSON.stringify({
       uri: jobURL,
       tests: ['http'],
-      sources: ['newyork', 'singapore', 'amsterdam', 'seattle', 'sydney', 'bangkok', 'tokyo', 'manila'],
+      sources: sources,
       options: {
         http: {
-          'method': 'HEAD', 'max-redirects': 4
+          'method': 'GET', 'max-redirects': 4
         }
       }
     })
   };
-  scheduleJob('https://api.wheresitup.com/v4/jobs', options);
-}
-
-// Fetch results of a job
-if (jobID !== undefined && jobID !== null) {
-  const options = {
+  const wheresitupResultOptions = {
     headers: {
       'Auth': 'Bearer ' + id + ' ' + token
     }
   };
-  fetchResults('https://api.wheresitup.com/v4/jobs', options, jobID, redisKey);
-}
 
-// If we are here it is an error
-
-
-function status(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return Promise.resolve(response)
-  } else {
-    return Promise.reject(new Error(response.statusText))
+  // Schedule the job and get the jobID
+  let jobID = await startJob(wheresitupURL, wheresitupStartOptions);
+  console.log('jobID:', jobID);
+  if (jobID === undefined || jobID === null) {
+    return;
   }
+
+  // Set a timeout
+  let myTimeout = setTimeout(async function () {
+    clearInterval(myInterval);
+    console.log('Timed out after 30 sec')
+  }, 30000);
+
+  // Poll for results at 1 sec intervals
+  let json = '';
+  let myInterval = setInterval(async function () {
+    json = await getResults(wheresitupURL, wheresitupResultOptions, jobID);
+    let complete = Object.keys(json.response.complete).toString();
+    let error = Object.keys(json.response.error).toString();
+    let inprogr = Object.keys(json.response.in_progress).toString();
+    console.log('Complete:', complete, '/ In progress:', inprogr, '/ Error:', error);
+    if (inprogr.length === 0) {
+      clearInterval(myInterval);
+      clearTimeout(myTimeout);
+      let res = buildResults(json);
+      console.log(res)
+      saveResults(res);
+    }
+  }, 1000);
 }
 
-function json(response) {
-  return response.json()
+//
+// Convert the JSON of completed tests to something a bit more compact
+//
+function buildResults(json) {
+  // list of completed cities
+  let cities = Object.keys(json.response.complete);
+
+  // build the result JSON
+  let result = {
+    url: json.request.url,
+    time: json.request.easy_time,
+    cities: []
+  };
+
+  // step through all completed cities, build summary for each city
+  while (cities.length) {
+    let city = cities.shift();
+    let summary = json.response.complete[city].http.summary
+    let citySummary = {
+      location: city,
+      contentLength: 0,
+      redirects: summary.length - 1,
+      timingConnected: Math.trunc(1000 * summary[0].timingConnected),
+      timingResponse: 0,
+      timingRequest: 0,
+      timingTransfer: 0
+    }
+    while (summary.length) {
+      let s = summary.pop();
+      citySummary.contentLength = s.responseCode == 200 ? s.contentLength : citySummary.contentLength;
+      citySummary.timingResponse += Number(s.timingResponse);
+      citySummary.timingRequest += Number(s.timingRequest);
+      citySummary.timingTransfer += Number(s.timingTransfer);
+    }
+    citySummary.timingResponse = Math.trunc(1000 * citySummary.timingResponse);
+    citySummary.timingRequest = Math.trunc(1000 * citySummary.timingRequest);
+    citySummary.timingTransfer = Math.trunc(1000 * citySummary.timingTransfer);
+    result.cities.push(citySummary);
+  }
+  return result;
 }
 
+//
+// Fetch the results of a completed job
+//
+async function getResults(url, options, jobID) {
+  const response = await fetch(url + '/' + jobID, options);
+  const json = await response.json();
+  console.log('Request: ', json.request.url, moment(json.request.easy_time).format(momFmt));
+  return json;
+}
 
-function fetchResults(url, options, jobID, redisKey) {
-  console.log('url:', url)
+//
+// Schedule a new job, return jobID
+//
+async function startJob(url, options) {
+  const response = await fetch(url, options);
+  const json = await response.json();
+  return json.jobID;
+}
 
+//
+// Store results to Redis
+//
+function saveResults(results, redisKey) {
   var redClient = redis.createClient();
   redClient.on('connect', function () {
     console.log(moment().format(momFmt) + ' Redis client connected');
@@ -88,88 +165,27 @@ function fetchResults(url, options, jobID, redisKey) {
     console.log(moment().format(momFmt) + ' Redis error:' + err);
   });
 
-  fetch(url + '/' + jobID, options)
-    .then(status)
-    .then(json)
-    .then(res => {
-      console.log('Request: ', res.request.url, res.request.ip, moment(res.request.easy_time).format(momFmt));
-      let cities = Object.keys(res.response.complete);
+  // Store key/value pair to Redis
+  let redisValue = JSON.stringify(results);
+  console.log(moment().format(momFmt) +
+    ' Storing ' + redisValue.length +
+    ' bytes, key=' + redisKey +
+    ' value=' + redisValue.substring(0, 60));
+  /*
+  redClient.set(redisKey, redisValue, function (error, result) {
+    if (result) {
+      console.log(moment().format(momFmt) + ' Result:' + result);
+    } else {
+      console.log(moment().format(momFmt) + ' Error: ' + error);
+    }
+  });
+  */
+  // exit
+  setInterval((() => {
+    process.exit();
+  }), 1000)
 
-      let result = {
-        url: res.request.url,
-        ip: res.request.ip,
-        time: res.request.easy_time,
-        cities: []
-      };
-
-      while (cities.length) {
-        let origin = cities.shift();
-        let summary = res.response.complete[origin].http.summary
-        console.log(summary)
-
-        let citySummary = {
-          origin: origin,
-          redirects: summary.length - 1,
-          timingConnected: Math.trunc(1000 * summary[0].timingResponse),
-          timingResponse: 0,
-          timingRequest: 0,
-          timingTransfer: 0
-        }
-
-        while (summary.length) {
-          let s = summary.pop();
-          citySummary.timingResponse += Number(s.timingResponse);
-          citySummary.timingRequest += Number(s.timingRequest);
-          citySummary.timingTransfer += Number(s.timingTransfer);
-        }
-        citySummary.timingResponse = Math.trunc(1000 * citySummary.timingResponse);
-        citySummary.timingRequest = Math.trunc(1000 * citySummary.timingRequest);
-        citySummary.timingTransfer = Math.trunc(1000 * citySummary.timingTransfer);
-        result.cities.push(citySummary);
-      }
-
-      console.log(result)
-
-      process.exit(0);
-
-      // Store key/value pair to Redis
-      let redisValue = JSON.stringify(results);
-      console.log(moment().format(momFmt) +
-        ' Storing ' + redisValue.length +
-        ' bytes, key=' + redisKey +
-        ' value=' + redisValue.substring(0, 60));
-
-      redClient.set(redisKey, redisValue, function (error, result) {
-        if (result) {
-          console.log(moment().format(momFmt) + ' Result:' + result);
-        } else {
-          console.log(moment().format(momFmt) + ' Error: ' + error);
-        }
-      });
-
-      // exit
-      setInterval((() => {
-        process.exit();
-      }), 1000)
-    })
-    .catch(err => {
-      console.log('Error fetching results:', err);
-      process.exit(1);
-    })
 }
 
-function scheduleJob(url, options) {
-  console.log('url', url);
-  console.log('options:', options);
-  fetch(url, options)
-    .then(status)
-    .then(json)
-    .then(res => {
-      console.log('Job ID: ', res.jobID)
-      process.exit(0);
-    })
-    .catch(err => {
-      console.log(err);
-      process.exit(1);
-    })
-}
+
+// EOF //
